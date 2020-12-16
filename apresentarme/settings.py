8b0937e0ffc4ede2b105fd1d1c9b5a8e6d216188
Paulo Matos <paulo.matos@fpf.br>
2020-12-16 18:57:26 -0400

    finish tutorial setup

--- apresentarme/settings.py
+++ apresentarme/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apresentarme.main'
+    'apresentarme.main',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
